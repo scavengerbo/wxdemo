52f64ae1d2aee6faa2363e5e3494af8542830826
tianxiaod <756851806@qq.com>
2022-03-11 15:23:33 +0800

    修改传参

--- src/main/java/com/example/wxdemo/controller/LoginController.java
+++ src/main/java/com/example/wxdemo/controller/LoginController.java
@@ -5,10 +5,7 @@ import com.example.wxdemo.service.ILoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -27,7 +24,7 @@ public class LoginController {
     }
 
     @PostMapping("/verifyNameAndPasswd")
-    public ReturnData verifyNameAndPasswd(@RequestBody Map<String, String> params) {
+    public ReturnData verifyNameAndPasswd(@RequestParam Map<String, String> params) {
 
         String name = params.get("name");
         String passwd = params.get("passwd");
