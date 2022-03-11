184ecee74b35469164c07b42e573a87db277030d
scavengerbo <87114469+scavengerbo@users.noreply.github.com>
2022-03-11 09:31:37 +0800

    提交启动类

--- src/main/java/com/example/wxdemo/WxdemoApplication.java
+++ src/main/java/com/example/wxdemo/WxdemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.wxdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.wxdemo.db.mapper")
 public class WxdemoApplication {
 
     public static void main(String[] args) {
